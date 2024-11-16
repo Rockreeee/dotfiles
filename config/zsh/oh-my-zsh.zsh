3e937a05f7172758d2e91517face7d6b74d21e0e
rockreeee <mmakt122@gamil.com>
2024-11-16 14:29:11 +0900

    pathを変更

--- config/zsh/oh-my-zsh.zsh
+++ config/zsh/oh-my-zsh.zsh
@@ -8,10 +8,10 @@ plugins=(
 source $ZSH/oh-my-zsh.sh
 
 # zsh-autosuggestions
-source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # zsh-syntax-highlighting
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
